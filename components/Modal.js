aa9643da610e7e30cd20ea01be287201662a6a87
syntaxerror403 <eliserrano2@gmail.com>
2022-04-30 06:17:10 -0400

    All Nighter Update

---
+++ components/Modal.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Button, Text, View } from "react-native";
+import Modal from "react-native-modal";
+
+function ModalTester() {
+  
+}
+
+export default ModalTester;
